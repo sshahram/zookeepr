565d2dd12d2ee70831fcdcbee6862f815ec84afe
sshahram <shirin.shahram23@gmail.com>
2021-04-07 23:37:04 -0700

    finish post functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,6 +26,26 @@ const handleAnimalFormSubmit = event => {
   }
   const animalObject = { name, species, diet, personalityTraits };
 
+  // handle post requests to backend
+fetch('/api/animals', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(animalObject)
+})
+  .then(response => {
+    if(response.ok) {
+      return response.json();
+    }
+    alert('Error ' + response.statusText);
+  })
+  .then(postResponse => {
+    console.log(postResponse);
+    alert('Thank you for adding an animal!');
+  });
+
 };
 
 $animalForm.addEventListener('submit', handleAnimalFormSubmit);
