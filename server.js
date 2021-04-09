7e1a0d34dcd79102ae87e7af334dd2bf06b317a3
sshahram <shirin.shahram23@gmail.com>
2021-04-09 15:38:44 -0700

    move all routes to a separate file

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// dependencies
+// dependencies for routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -17,32 +22,13 @@ app.use(express.json());
 // instruct the server to make certain files readily available
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 // add routes
 // app.get('/',(req, res) => {
 //     res.json({message: 'Hello World!'})
 // });
-
-// route to serve index.html page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// route to serve animals.html
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-// route to serve zookeepers.html
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-// If client makes a request for a route that doesn't exist
-// for example /about
-// They will be redirected to the homepage
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 // to make our server listen
 app.listen(PORT, () => {
